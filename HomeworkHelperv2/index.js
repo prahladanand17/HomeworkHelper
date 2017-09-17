f534d780d1c5fba84f3ae8d98500f7d54c2a45b8
prahladanand17 <anprahlad@gmail.com>
2017-09-17 11:45:52 -0400

    final hackathon implementation; client side and server side js functional, need to push data to heroku DB

--- HomeworkHelperv2/index.js
+++ HomeworkHelperv2/index.js
@@ -1,12 +1,26 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const pg = require('pg');
+const { Client, Pool } = require('pg');
+const cors = require('cors');
 
+const client = new Client({
+    connectionString: 'postgres://fskxuwlapijkqn:42c90b3eaad0ef15e0360094b4e5c94b9864d0872784c5ecbd96cff105e5f0e4@ec2-107-20-255-96.compute-1.amazonaws.com:5432/d1fh9mu9ubk340?ssl=true',
+});
+
+client.connect();
+
+client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+    if (err) throw err;
+    for (let row of res.rows) {
+        console.log(JSON.stringify(row));
+    }
+    client.end();
+});
 
 (async function () {
     const app = express();
     const connections = [];
-    const pool = new pg.Pool();
+    const pool = new Pool();
 
     async function query(q) {
         return new Promise(function (resolve, reject) {
@@ -22,31 +36,21 @@ const pg = require('pg');
     }
 
 
-    async function getFunctions() {
-        return await query("SELECT * FROM subject")
+    async function getFunctions(subject) {
+        return await query("SELECT * FROM paldata where subject='"+ subject+"'")
     }
+
+    await query("CREATE TABLE IF NOT EXISTS PalData (subject" + " VARCHAR(50), formula VARCHAR(500))");
 
 
     app.use(express.static('.'));
     app.use(bodyParser.json());
+    app.use(cors());
+    app.options('*', cors());
 
-    app.put('/register/:subject', async function (req, res) {
-        res.status(200).send(await registerUser(req.params.nick))
+    app.get('/functions/:subject', async function (req, res) {
+        res.status(200).send(await getFunctions(req.param.subject))
     });
-
-    app.get('/messages', function (req, res) {
-        res.writeHead(200, {
-            'Content-Type': 'text/event-stream',
-            'Cache-Control': 'no-cache',
-            'Connection': 'keep-alive'
-        });
-
-        res.sendEvent = function (type, data) {
-            res.write(`event: ${type}\ndata: ${JSON.stringify(data).split('\n').join('\ndata: ')}\n\n`);
-        };
-        connections.push(res);
-    });
-
     app.listen(8080);
 
 })();
--- HomeworkHelperv2/lib/client.js
+++ HomeworkHelperv2/lib/client.js
@@ -1,78 +1,27 @@
-// const message = m => `<div class="message card">
-// 							<div class="card-content">
-// 								<span class="card-title light-blue-text">${m.author}</span>
-// 								<p>
-// 									${m.content.split(' ').map(s => s[0] === '/' ?
-// 										`<span style="color: #888">${s}</span>` : s).join(' ')}
-// 								</p>
-// 							</div>
-// 						</div>`;
-//
-// const table = l => `<div class="message card">
-// 							<div class="card-content">
-// 								<span class="card-title light-blue-text">${l[0]}</span>
-// 								<table class="striped">
-// 									<thead>
-// 										<tr>
-// 											<th>Name</th>
-// 											<th>Number of Parrots</th>
-// 										</tr>
-// 									</thead>
-// 									<tbody>
-// 										${l.splice(1).reduce(function(acc, r) (`
-// 											${acc}
-// 											<tr>
-// 												<td>${r.split(': ')[0]}</td>
-// 												<td>${r.split(': ')[1]}</td>
-// 											</tr>
-// 										`), '')}
-// 									</tbody>
-//
-// 								</table>
-// 							</div>
-// 						</div>`;
+function initSearch() {
+    $('#message-form').submit(function() {
+        let $subject = $('#subject');
+        let val = $subject.val();
 
-function initSearch(subject) {
-    const server = new EventSource('/messages');
-    $.ajax({
-        type: 'PUT',
-        url: `/register/${subject}`
-    });
-
-    $('#message').focus();
-    $('#message-form').submit(() => {
-        let $message = $('#message');
-
-        if ($message.val()) {
+        if (val) {
             $.ajax({
-                method: 'PUT',
-                url: '/message',
-                contentType: 'application/json',
-                data: JSON.stringify({
-                    author: nick,
-                    content: $message.val()
-                })
+                method: 'GET',
+                url: `http://localhost:8080/functions/${val}`,
             });
-            $message.val('');
-            $message.focus();
         }
 
         return false;
     });
-
-
-    server.addEventListener('message', e => addMessage(JSON.parse(e.data)));
-    server.addEventListener('functions', e => addLeaderboard(JSON.parse(e.data).content));
 }
 
-$(document).ready(() => {
+$(document).ready(function() {
     console.log('test');
     const $nickModal = $('.modal');
     let $nickInput = $('#nick');
 
     $nickModal.modal({
         dismissible: false,
-        complete: () => initSearch($nickInput.val())
+        ready: () => initSearch()
     });
     $nickModal.modal('open');
     $nickInput.focus();
